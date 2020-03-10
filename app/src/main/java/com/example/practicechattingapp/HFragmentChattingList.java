eb82cd49c8d666a36c1a47211dc6c9d05e9f7877
wnsgk42 <57088799+wnsgk42@users.noreply.github.com>
2020-03-10 21:16:53 +0900

    chattinglist cardview setting,FragmentChattingList basic setting finish

---
+++ app/src/main/java/com/example/practicechattingapp/HFragmentChattingList.java
@@ -0,0 +1,30 @@
+package com.example.practicechattingapp;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class HFragmentChattingList extends Fragment {
+
+    private RecyclerView hRecyclerViewChattingList; //채팅목록 recyclerview 참조 변수
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        ViewGroup rootview =(ViewGroup) inflater.inflate(R.layout.h_fragment_chatting_list, container,false);
+        hRecyclerViewChattingList = rootview.findViewById(R.id.hRecyclerViewChattingList);
+
+        hRecyclerViewChattingList.setHasFixedSize(true);
+
+        return rootview;
+    }
+}
